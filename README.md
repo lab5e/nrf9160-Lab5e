8a9d68d56fa78a6c313b232c101b7952d5caa3f5
Hans Jørgen Grimstad <hansj66@gmail.com>
2021-05-19 15:06:57 +0200

    Updated Hello World sample
    * Updated APN to mda.lab5e
    * Updated CMakeLists.txt
    * Updated README with new building instructions
    * Updated license

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
-# Lab5e nrf9160 samples
+# Getting started
 
-Work in progress...
+Open the toolchain manager from nRFConnect and open a shell for the selected toolchain
+
+## Hello World
+1) Open west.yml and add the following lines in the remotes section:
+
+```bash
+    - name: lab5e
+      url-base: https://github.com/lab5e
+```
+
+2) Add the following to the projects section:
+
+projects:
+```bash
+    - name: nrf9160-Lab5e
+      remote: lab5e
+```
+
+3) Save the file and run 
+```bash
+west update
+```
+
+The folder nrf9160-Lab5e should now appear under ncs\<toolchain version>\nrf\applications
+
+4) Build a sample from the applications folder
+
+nRF9160DK:
+
+```bash
+west build nrf9160-Lab5e\samples\hello_world -b nrf9160dk_nrf9160ns -p
+```
+
+Thingy91:
+
+```bash
+west build nrf9160-Lab5e\samples\hello_world -b thingy91_nrf9160ns -p
+```
+
+5) Flash the sample by running
+```bash
+west flash
+```
+from the applications folder.
+
+6) Open up JLink RTT Viewer in order to view console output and view IMEI/IMSI necessary for registering your device in [Span](https://span.lab5e.com/).
+
+7) Register your device in Span and reboot in order to send a hello world message.
+
+## COAP
+WIP...
+
+## FOTA
+WIP...
+
+
+
+
+
+
+
+
+
+
+
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -1,55 +1,78 @@
 /*
- * Copyright (c) 2019 Exploratory Engineering
- */
-#include <zephyr.h>
+	Copyright [2019] [Exploratory Engineering]
+	Modifications Copyright [2021] [Lab5e AS]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.#include <zephyr.h>
+*/   
+
 #include <errno.h>
 #include <stdio.h>
 #include <net/socket.h>
-#include <lte_lc.h>
-#include <modem_info.h>
+#include <modem/lte_lc.h>
+#include <modem/modem_info.h>
 
-bool print_imei_imsi() {
-	if (modem_info_init() != 0) {
+bool print_imei_imsi() 
+{
+	if (modem_info_init() != 0) 
+	{
 		printf("Modem info could not be initialized");
 		return false;
 	}
 
 	int ret = lte_lc_normal();
-	if (ret != 0) {
+	if (ret != 0) 
+	{
 		printf("lte_lc_normal: %d\n", ret);
 		return false;
 	}
 
 	while (true) {
-		u16_t uicc_state;
+		uint16_t uicc_state;
 		int len = modem_info_short_get(MODEM_INFO_UICC, &uicc_state);
-		if (len <= 0) {
+		if (len <= 0) 
+		{
 			printf("Failed reading UICC: %d\n", len);
 			return false;
 		}
-		if (uicc_state == 1) {
+		if (uicc_state == 1) 
+		{
 			break;
 		}
 		k_sleep(K_SECONDS(1));
 	}
 
-	u8_t buf[32] = {0};
-	int len = modem_info_string_get(MODEM_INFO_IMEI, buf);
-	if (len <= 0) {
+	const int BUFFER_SIZE = 32;
+	uint8_t buf[BUFFER_SIZE];
+	memset(buf, 0, BUFFER_SIZE);
+	int len = modem_info_string_get(MODEM_INFO_IMEI, buf,BUFFER_SIZE);
+	if (len <= 0) 
+	{
 		printf("Failed reading IMEI: %d\n", len);
 		return false;
 	}
 	printf("IMEI: %s\n", buf);
 
-	len = modem_info_string_get(MODEM_INFO_IMSI, buf);
-	if (len <= 0) {
+	len = modem_info_string_get(MODEM_INFO_IMSI, buf, BUFFER_SIZE);
+	if (len <= 0) 
+	{
 		printf("Failed reading IMSI: %d\n", len);
 		return false;
 	}
 	printf("IMSI: %s\n", buf);
 
 	ret = lte_lc_offline();
-	if (ret != 0) {
+	if (ret != 0) 
+	{
 		printf("lte_lc_offline: %d\n", ret);
 		return false;
 	}
@@ -57,20 +80,24 @@ bool print_imei_imsi() {
 	return true;
 }
 
-bool send_message(const char *message) {
+bool send_message(const char *message) 
+{
 	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock < 0) {
+	if (sock < 0) 
+	{
 		printf("Error opening socket: %d\n", errno);
 		return false;
 	}
 
-	static struct sockaddr_in remote_addr = {
+	static struct sockaddr_in remote_addr = 
+	{
 		sin_family: AF_INET,
 		sin_port:   htons(1234),
 	};
 	net_addr_pton(AF_INET, "172.16.15.14", &remote_addr.sin_addr);
 
-	if (sendto(sock, message, strlen(message), 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr)) < 0) {
+	if (sendto(sock, message, strlen(message), 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr)) < 0) 
+	{
 		printf("Error sending: %d\n", errno);
 		close(sock);
 		return false;
@@ -80,7 +107,8 @@ bool send_message(const char *message) {
 	return true;
 }
 
-void main() {
+void main() 
+{
 	printf("Example application started\n"); 
 
 	int err = lte_lc_init();
@@ -93,12 +121,14 @@ void main() {
 
 	printf("Connecting...\n"); 
 	err = lte_lc_connect();
-	if (err != 0) {
+	if (err != 0) 
+	{
 		printf("lte_lc_connect failed: %d\n", err);
 		return;
 	}
 
-	if (send_message("Hello, World!")) {
+	if (send_message("Hello, World!")) 
+	{
 		printf("Message sent!\n"); 
 	} else {
 		printf("Failed to send\n");
